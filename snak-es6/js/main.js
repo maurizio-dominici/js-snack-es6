227a021ea408831d91c7a02c87915a1b29f39784
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-14 15:50:50 +0200

    add array

---
+++ snak-es6/js/main.js
@@ -0,0 +1,26 @@
+// ?_________________________ SNACK 1____________________________________
+
+// ^ METODO 1
+
+// let bicycles = [];
+
+// const rideBicycle = {
+//   name: "specialized",
+//   weight: 10,
+// };
+
+// bicycles.push(rideBicycle);
+// console.log(bicycles);
+
+// ^ METODO 2
+
+const bicycles = [
+  { name: "Scott", weight: 10 },
+  { name: "Cannodale", weight: 9 },
+  { name: "Bianchi", weight: 8 },
+  { name: "Orbea", weight: 10 },
+  { name: "Santa Cruz", weight: 11 },
+  { name: "specialized", weight: 9 },
+];
+
+for (let i = 0; i < bicycles.length; i++) {}
